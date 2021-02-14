162d8c31308697abbe0c3d4b7e1e9760da4c266a
Hanhui LI <hanhui.li@gmail.com>
2021-02-14 20:29:11 +0800

    host config - more options

--- wolverine-container/wolverine-container-docker/src/main/java/io/wolverine/container/docker/HostConfig.java
+++ wolverine-container/wolverine-container-docker/src/main/java/io/wolverine/container/docker/HostConfig.java
@@ -1,7 +1,42 @@
 package io.wolverine.container.docker;
 
+import java.util.List;
+
+import com.github.dockerjava.api.model.Ulimit;
+
 public class HostConfig {
+	private com.github.dockerjava.api.model.HostConfig hostConfig;
+	public static HostConfig builder() {
+		return new HostConfig();
+	}
+	private HostConfig() {
+		this.hostConfig = new com.github.dockerjava.api.model.HostConfig();
+	}
 	HostConfig withCpuCount(Long cpuCount) {
 		return this;
 	}
+	HostConfig withDiskQuota(Long diskQuota) {
+		return this;
+	}
+	HostConfig withMemory(Long memory){
+		return this;
+	}
+	HostConfig withMemorySwap(Long memorySwap) {
+		return this;
+	}
+	HostConfig withDns(String... dns) {
+        return this;
+    }
+    HostConfig withDns(List<String> dns) {
+    	return this;
+    }
+    HostConfig withUlimits(Ulimit[] ulimits) {
+        return this;
+    }
+    public HostConfig withUlimits(List<Ulimit> ulimits) {
+    	return this;
+    }
+    public HostConfig build() {
+    	return this;
+    }
 }
