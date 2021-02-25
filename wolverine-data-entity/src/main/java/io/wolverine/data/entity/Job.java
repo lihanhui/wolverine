8f54cfd0f69af6732e4dbb51f77a76193ba97f69
Hanhui LI <hanhui.li@gmail.com>
2021-02-25 20:51:40 +0800

    job entity

--- wolverine-data-entity/src/main/java/io/wolverine/data/entity/Job.java
+++ wolverine-data-entity/src/main/java/io/wolverine/data/entity/Job.java
@@ -1,5 +1,21 @@
 package io.wolverine.data.entity;
 
-public class Job {
+import java.util.Map;
 
+public class Job {
+	private String  jobId;
+	private Integer tasks;
+	private Integer runningTasks;
+	private Integer mem;
+	private Integer cores;
+	private Integer disk;
+	private Integer mode;
+	private Integer status;
+	private Integer taskType;
+	private String imageUri; // Or image-and-tag
+	private String entryPoint; // for docker or simple command
+	private String target;
+	private Map<String, String> options;
+	private Long createDate;
+	private Long updateDate;
 }
