3976e542d0a82627ef2e9290e1da1446e639a3f4
Hanhui LI <hanhui.li@gmail.com>
2021-02-25 21:05:09 +0800

    message and controller

--- wolverine-scheduler/src/main/java/io/wolverine/scheduler/controller/WolverineJobController.java
+++ wolverine-scheduler/src/main/java/io/wolverine/scheduler/controller/WolverineJobController.java
@@ -2,45 +2,57 @@ package io.wolverine.scheduler.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 
 import io.doraemon.restful.ResultMsg;
+import io.wolverine.message.job.JobIdMsg;
+import io.wolverine.message.job.QueryJobMsg;
+import io.wolverine.message.job.SetJobModeMsg;
+import io.wolverine.message.job.SubmitJobMsg;
 
 @RestController
 public class WolverineJobController {
 	@RequestMapping(value="/wolverine/scheduler/job/submitJob", method=RequestMethod.POST)
-    public DeferredResult<ResultMsg> submitJob(/*@RequestBody SubmitUpdateMsg submitUpdateMsg,*/
+    public DeferredResult<ResultMsg> submitJob(@RequestBody SubmitJobMsg msg,
     		HttpServletRequest request) {
 		DeferredResult<ResultMsg> dr = new DeferredResult<ResultMsg>();
 		dr.setResult(ResultMsg.ok());
 		return dr;
 	}
 	@RequestMapping(value="/wolverine/scheduler/job/stopJob", method=RequestMethod.POST)
-    public DeferredResult<ResultMsg> stopJob(/*@RequestBody SubmitUpdateMsg submitUpdateMsg,*/
+    public DeferredResult<ResultMsg> stopJob(@RequestBody JobIdMsg msg,
     		HttpServletRequest request) {
 		DeferredResult<ResultMsg> dr = new DeferredResult<ResultMsg>();
 		dr.setResult(ResultMsg.ok());
 		return dr;
 	}
 	@RequestMapping(value="/wolverine/scheduler/job/starJob", method=RequestMethod.POST)
-    public DeferredResult<ResultMsg> starJob(/*@RequestBody SubmitUpdateMsg submitUpdateMsg,*/
+    public DeferredResult<ResultMsg> starJob(@RequestBody JobIdMsg msg,
     		HttpServletRequest request) {
 		DeferredResult<ResultMsg> dr = new DeferredResult<ResultMsg>();
 		dr.setResult(ResultMsg.ok());
 		return dr;
 	}
 	@RequestMapping(value="/wolverine/scheduler/job/deleteJob", method=RequestMethod.POST)
-    public DeferredResult<ResultMsg> deleteJob(/*@RequestBody SubmitUpdateMsg submitUpdateMsg,*/
+    public DeferredResult<ResultMsg> deleteJob(@RequestBody JobIdMsg msg,
+    		HttpServletRequest request) {
+		DeferredResult<ResultMsg> dr = new DeferredResult<ResultMsg>();
+		dr.setResult(ResultMsg.ok());
+		return dr;
+	}
+	@RequestMapping(value="/wolverine/scheduler/job/setJobMode", method=RequestMethod.POST)
+    public DeferredResult<ResultMsg> setJobMode(@RequestBody SetJobModeMsg msg,
     		HttpServletRequest request) {
 		DeferredResult<ResultMsg> dr = new DeferredResult<ResultMsg>();
 		dr.setResult(ResultMsg.ok());
 		return dr;
 	}
 	@RequestMapping(value="/wolverine/scheduler/job/queryJob", method=RequestMethod.POST)
-    public DeferredResult<ResultMsg> queryJob(/*@RequestBody SubmitUpdateMsg submitUpdateMsg,*/
+    public DeferredResult<ResultMsg> queryJob(@RequestBody QueryJobMsg msg,
     		HttpServletRequest request) {
 		DeferredResult<ResultMsg> dr = new DeferredResult<ResultMsg>();
 		dr.setResult(ResultMsg.ok());
--- wolverine-scheduler/src/main/java/io/wolverine/scheduler/controller/WolverineTaskController.java
+++ wolverine-scheduler/src/main/java/io/wolverine/scheduler/controller/WolverineTaskController.java
@@ -2,45 +2,57 @@ package io.wolverine.scheduler.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 
 import io.doraemon.restful.ResultMsg;
+import io.wolverine.message.task.QueryTaskMsg;
+import io.wolverine.message.task.SetTaskModeMsg;
+import io.wolverine.message.task.SubmitTaskMsg;
+import io.wolverine.message.task.TaskIdMsg;
 
 @RestController
 public class WolverineTaskController {
 	@RequestMapping(value="/wolverine/scheduler/task/restartTask", method=RequestMethod.POST)
-    public DeferredResult<ResultMsg> restartTask(/*@RequestBody SubmitUpdateMsg submitUpdateMsg,*/
+    public DeferredResult<ResultMsg> restartTask(@RequestBody TaskIdMsg msg,
     		HttpServletRequest request) {
 		DeferredResult<ResultMsg> dr = new DeferredResult<ResultMsg>();
 		dr.setResult(ResultMsg.ok());
 		return dr;
 	}
 	@RequestMapping(value="/wolverine/scheduler/task/killTask", method=RequestMethod.POST)
-    public DeferredResult<ResultMsg> killTask(/*@RequestBody SubmitUpdateMsg submitUpdateMsg,*/
+    public DeferredResult<ResultMsg> killTask(@RequestBody TaskIdMsg msg,
     		HttpServletRequest request) {
 		DeferredResult<ResultMsg> dr = new DeferredResult<ResultMsg>();
 		dr.setResult(ResultMsg.ok());
 		return dr;
 	}
 	@RequestMapping(value="/wolverine/scheduler/task/startTask", method=RequestMethod.POST)
-    public DeferredResult<ResultMsg> startTask(/*@RequestBody SubmitUpdateMsg submitUpdateMsg,*/
+    public DeferredResult<ResultMsg> startTask(@RequestBody TaskIdMsg msg,
     		HttpServletRequest request) {
 		DeferredResult<ResultMsg> dr = new DeferredResult<ResultMsg>();
 		dr.setResult(ResultMsg.ok());
 		return dr;
 	}
 	@RequestMapping(value="/wolverine/scheduler/task/submitTask", method=RequestMethod.POST)
-    public DeferredResult<ResultMsg> submitTask(/*@RequestBody SubmitUpdateMsg submitUpdateMsg,*/
+    public DeferredResult<ResultMsg> submitTask(@RequestBody SubmitTaskMsg msg,
+    		HttpServletRequest request) {
+		DeferredResult<ResultMsg> dr = new DeferredResult<ResultMsg>();
+		dr.setResult(ResultMsg.ok());
+		return dr;
+	}
+	@RequestMapping(value="/wolverine/scheduler/task/setTaskMode", method=RequestMethod.POST)
+    public DeferredResult<ResultMsg> setTaskMode(@RequestBody SetTaskModeMsg msg,
     		HttpServletRequest request) {
 		DeferredResult<ResultMsg> dr = new DeferredResult<ResultMsg>();
 		dr.setResult(ResultMsg.ok());
 		return dr;
 	}
 	@RequestMapping(value="/wolverine/scheduler/task/queryTask", method=RequestMethod.POST)
-    public DeferredResult<ResultMsg> queryTask(/*@RequestBody SubmitUpdateMsg submitUpdateMsg,*/
+    public DeferredResult<ResultMsg> queryTask(@RequestBody QueryTaskMsg msg,
     		HttpServletRequest request) {
 		DeferredResult<ResultMsg> dr = new DeferredResult<ResultMsg>();
 		dr.setResult(ResultMsg.ok());
