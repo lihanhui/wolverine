52621a0f407d7beb9feb635276e6c174140dadcd
Hanhui LI <hanhui.li@gmail.com>
2021-03-05 20:45:04 +0800

    entity - getter and setter

--- wolverine-data-entity/src/main/java/io/wolverine/data/entity/Job.java
+++ wolverine-data-entity/src/main/java/io/wolverine/data/entity/Job.java
@@ -23,4 +23,95 @@ public class Job {
 	private Map<String, String> options;
 	private Long createDate;
 	private Long updateDate;
+	public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	public Integer getTasks() {
+		return tasks;
+	}
+	public void setTasks(Integer tasks) {
+		this.tasks = tasks;
+	}
+	public Integer getRunningTasks() {
+		return runningTasks;
+	}
+	public void setRunningTasks(Integer runningTasks) {
+		this.runningTasks = runningTasks;
+	}
+	public Integer getMem() {
+		return mem;
+	}
+	public void setMem(Integer mem) {
+		this.mem = mem;
+	}
+	public Integer getCores() {
+		return cores;
+	}
+	public void setCores(Integer cores) {
+		this.cores = cores;
+	}
+	public Integer getDisk() {
+		return disk;
+	}
+	public void setDisk(Integer disk) {
+		this.disk = disk;
+	}
+	public Integer getMode() {
+		return mode;
+	}
+	public void setMode(Integer mode) {
+		this.mode = mode;
+	}
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public Integer getTaskType() {
+		return taskType;
+	}
+	public void setTaskType(Integer taskType) {
+		this.taskType = taskType;
+	}
+	public String getImageUri() {
+		return imageUri;
+	}
+	public void setImageUri(String imageUri) {
+		this.imageUri = imageUri;
+	}
+	public String getEntryPoint() {
+		return entryPoint;
+	}
+	public void setEntryPoint(String entryPoint) {
+		this.entryPoint = entryPoint;
+	}
+	public String getTarget() {
+		return target;
+	}
+	public void setTarget(String target) {
+		this.target = target;
+	}
+	public Map<String, String> getOptions() {
+		return options;
+	}
+	public void setOptions(Map<String, String> options) {
+		this.options = options;
+	}
+	public Long getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Long createDate) {
+		this.createDate = createDate;
+	}
+	public Long getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Long updateDate) {
+		this.updateDate = updateDate;
+	}
+	
 }
--- wolverine-data-entity/src/main/java/io/wolverine/data/entity/Task.java
+++ wolverine-data-entity/src/main/java/io/wolverine/data/entity/Task.java
@@ -15,4 +15,52 @@ public class Task {
 	private Integer mode;
 	private Long createDate;
 	private Long updateDate;
+	public String getTaskId() {
+		return taskId;
+	}
+	public void setTaskId(String taskId) {
+		this.taskId = taskId;
+	}
+	public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	public String getContainerId() {
+		return containerId;
+	}
+	public void setContainerId(String containerId) {
+		this.containerId = containerId;
+	}
+	public String getProcessId() {
+		return processId;
+	}
+	public void setProcessId(String processId) {
+		this.processId = processId;
+	}
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public Integer getMode() {
+		return mode;
+	}
+	public void setMode(Integer mode) {
+		this.mode = mode;
+	}
+	public Long getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Long createDate) {
+		this.createDate = createDate;
+	}
+	public Long getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Long updateDate) {
+		this.updateDate = updateDate;
+	}
 }
