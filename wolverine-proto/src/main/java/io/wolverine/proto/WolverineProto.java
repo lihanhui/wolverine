67eb0ccd29089b010f9219f89480aa400975fe8a
Hanhui LI <hanhui.li@gmail.com>
2021-03-11 11:35:55 +0800

    to add ip: port

--- wolverine-proto/src/main/java/io/wolverine/proto/WolverineProto.java
+++ wolverine-proto/src/main/java/io/wolverine/proto/WolverineProto.java
@@ -3177,6 +3177,29 @@ public final class WolverineProto {
      */
     io.wolverine.proto.WolverineProto.LimitOrBuilder getLimitsOrBuilder(
         int index);
+
+    /**
+     * <code>optional int32 port = 12;</code>
+     */
+    boolean hasPort();
+    /**
+     * <code>optional int32 port = 12;</code>
+     */
+    int getPort();
+
+    /**
+     * <code>optional string ip = 13;</code>
+     */
+    boolean hasIp();
+    /**
+     * <code>optional string ip = 13;</code>
+     */
+    java.lang.String getIp();
+    /**
+     * <code>optional string ip = 13;</code>
+     */
+    com.google.protobuf.ByteString
+        getIpBytes();
   }
   /**
    * Protobuf type {@code CreateTaskMsg}
@@ -3202,6 +3225,8 @@ public final class WolverineProto {
       disk_ = 0;
       options_ = java.util.Collections.emptyList();
       limits_ = java.util.Collections.emptyList();
+      port_ = 0;
+      ip_ = "";
     }
 
     @java.lang.Override
@@ -3302,6 +3327,17 @@ public final class WolverineProto {
               }
               limits_.add(
                   input.readMessage(io.wolverine.proto.WolverineProto.Limit.PARSER, extensionRegistry));
+              break;
+            }
+            case 96: {
+              bitField0_ |= 0x00000200;
+              port_ = input.readInt32();
+              break;
+            }
+            case 106: {
+              com.google.protobuf.ByteString bs = input.readBytes();
+              bitField0_ |= 0x00000400;
+              ip_ = bs;
               break;
             }
           }
@@ -3726,6 +3762,63 @@ public final class WolverineProto {
       return limits_.get(index);
     }
 
+    public static final int PORT_FIELD_NUMBER = 12;
+    private int port_;
+    /**
+     * <code>optional int32 port = 12;</code>
+     */
+    public boolean hasPort() {
+      return ((bitField0_ & 0x00000200) == 0x00000200);
+    }
+    /**
+     * <code>optional int32 port = 12;</code>
+     */
+    public int getPort() {
+      return port_;
+    }
+
+    public static final int IP_FIELD_NUMBER = 13;
+    private volatile java.lang.Object ip_;
+    /**
+     * <code>optional string ip = 13;</code>
+     */
+    public boolean hasIp() {
+      return ((bitField0_ & 0x00000400) == 0x00000400);
+    }
+    /**
+     * <code>optional string ip = 13;</code>
+     */
+    public java.lang.String getIp() {
+      java.lang.Object ref = ip_;
+      if (ref instanceof java.lang.String) {
+        return (java.lang.String) ref;
+      } else {
+        com.google.protobuf.ByteString bs = 
+            (com.google.protobuf.ByteString) ref;
+        java.lang.String s = bs.toStringUtf8();
+        if (bs.isValidUtf8()) {
+          ip_ = s;
+        }
+        return s;
+      }
+    }
+    /**
+     * <code>optional string ip = 13;</code>
+     */
+    public com.google.protobuf.ByteString
+        getIpBytes() {
+      java.lang.Object ref = ip_;
+      if (ref instanceof java.lang.String) {
+        com.google.protobuf.ByteString b = 
+            com.google.protobuf.ByteString.copyFromUtf8(
+                (java.lang.String) ref);
+        ip_ = b;
+        return b;
+      } else {
+        return (com.google.protobuf.ByteString) ref;
+      }
+    }
+
     private byte memoizedIsInitialized = -1;
     public final boolean isInitialized() {
       byte isInitialized = memoizedIsInitialized;
@@ -3787,6 +3880,12 @@ public final class WolverineProto {
       for (int i = 0; i < limits_.size(); i++) {
         output.writeMessage(11, limits_.get(i));
       }
+      if (((bitField0_ & 0x00000200) == 0x00000200)) {
+        output.writeInt32(12, port_);
+      }
+      if (((bitField0_ & 0x00000400) == 0x00000400)) {
+        com.google.protobuf.GeneratedMessageV3.writeString(output, 13, ip_);
+      }
       unknownFields.writeTo(output);
     }
 
@@ -3832,6 +3931,13 @@ public final class WolverineProto {
       for (int i = 0; i < limits_.size(); i++) {
         size += com.google.protobuf.CodedOutputStream
           .computeMessageSize(11, limits_.get(i));
+      }
+      if (((bitField0_ & 0x00000200) == 0x00000200)) {
+        size += com.google.protobuf.CodedOutputStream
+          .computeInt32Size(12, port_);
+      }
+      if (((bitField0_ & 0x00000400) == 0x00000400)) {
+        size += com.google.protobuf.GeneratedMessageV3.computeStringSize(13, ip_);
       }
       size += unknownFields.getSerializedSize();
       memoizedSize = size;
@@ -3898,6 +4004,16 @@ public final class WolverineProto {
           .equals(other.getOptionsList());
       result = result && getLimitsList()
           .equals(other.getLimitsList());
+      result = result && (hasPort() == other.hasPort());
+      if (hasPort()) {
+        result = result && (getPort()
+            == other.getPort());
+      }
+      result = result && (hasIp() == other.hasIp());
+      if (hasIp()) {
+        result = result && getIp()
+            .equals(other.getIp());
+      }
       result = result && unknownFields.equals(other.unknownFields);
       return result;
     }
@@ -3952,6 +4068,14 @@ public final class WolverineProto {
       if (getLimitsCount() > 0) {
         hash = (37 * hash) + LIMITS_FIELD_NUMBER;
         hash = (53 * hash) + getLimitsList().hashCode();
+      }
+      if (hasPort()) {
+        hash = (37 * hash) + PORT_FIELD_NUMBER;
+        hash = (53 * hash) + getPort();
+      }
+      if (hasIp()) {
+        hash = (37 * hash) + IP_FIELD_NUMBER;
+        hash = (53 * hash) + getIp().hashCode();
       }
       hash = (29 * hash) + unknownFields.hashCode();
       memoizedHashCode = hash;
@@ -4114,6 +4238,10 @@ public final class WolverineProto {
         } else {
           limitsBuilder_.clear();
         }
+        port_ = 0;
+        bitField0_ = (bitField0_ & ~0x00000800);
+        ip_ = "";
+        bitField0_ = (bitField0_ & ~0x00001000);
         return this;
       }
 
@@ -4192,6 +4320,14 @@ public final class WolverineProto {
         } else {
           result.limits_ = limitsBuilder_.build();
         }
+        if (((from_bitField0_ & 0x00000800) == 0x00000800)) {
+          to_bitField0_ |= 0x00000200;
+        }
+        result.port_ = port_;
+        if (((from_bitField0_ & 0x00001000) == 0x00001000)) {
+          to_bitField0_ |= 0x00000400;
+        }
+        result.ip_ = ip_;
         result.bitField0_ = to_bitField0_;
         onBuilt();
         return result;
@@ -4324,6 +4460,14 @@ public final class WolverineProto {
               limitsBuilder_.addAllMessages(other.limits_);
             }
           }
+        }
+        if (other.hasPort()) {
+          setPort(other.getPort());
+        }
+        if (other.hasIp()) {
+          bitField0_ |= 0x00001000;
+          ip_ = other.ip_;
+          onChanged();
         }
         this.mergeUnknownFields(other.unknownFields);
         onChanged();
@@ -5444,6 +5588,114 @@ public final class WolverineProto {
           limits_ = null;
         }
         return limitsBuilder_;
+      }
+
+      private int port_ ;
+      /**
+       * <code>optional int32 port = 12;</code>
+       */
+      public boolean hasPort() {
+        return ((bitField0_ & 0x00000800) == 0x00000800);
+      }
+      /**
+       * <code>optional int32 port = 12;</code>
+       */
+      public int getPort() {
+        return port_;
+      }
+      /**
+       * <code>optional int32 port = 12;</code>
+       */
+      public Builder setPort(int value) {
+        bitField0_ |= 0x00000800;
+        port_ = value;
+        onChanged();
+        return this;
+      }
+      /**
+       * <code>optional int32 port = 12;</code>
+       */
+      public Builder clearPort() {
+        bitField0_ = (bitField0_ & ~0x00000800);
+        port_ = 0;
+        onChanged();
+        return this;
+      }
+
+      private java.lang.Object ip_ = "";
+      /**
+       * <code>optional string ip = 13;</code>
+       */
+      public boolean hasIp() {
+        return ((bitField0_ & 0x00001000) == 0x00001000);
+      }
+      /**
+       * <code>optional string ip = 13;</code>
+       */
+      public java.lang.String getIp() {
+        java.lang.Object ref = ip_;
+        if (!(ref instanceof java.lang.String)) {
+          com.google.protobuf.ByteString bs =
+              (com.google.protobuf.ByteString) ref;
+          java.lang.String s = bs.toStringUtf8();
+          if (bs.isValidUtf8()) {
+            ip_ = s;
+          }
+          return s;
+        } else {
+          return (java.lang.String) ref;
+        }
+      }
+      /**
+       * <code>optional string ip = 13;</code>
+       */
+      public com.google.protobuf.ByteString
+          getIpBytes() {
+        java.lang.Object ref = ip_;
+        if (ref instanceof String) {
+          com.google.protobuf.ByteString b = 
+              com.google.protobuf.ByteString.copyFromUtf8(
+                  (java.lang.String) ref);
+          ip_ = b;
+          return b;
+        } else {
+          return (com.google.protobuf.ByteString) ref;
+        }
+      }
+      /**
+       * <code>optional string ip = 13;</code>
+       */
+      public Builder setIp(
+          java.lang.String value) {
+        if (value == null) {
+    throw new NullPointerException();
+  }
+  bitField0_ |= 0x00001000;
+        ip_ = value;
+        onChanged();
+        return this;
+      }
+      /**
+       * <code>optional string ip = 13;</code>
+       */
+      public Builder clearIp() {
+        bitField0_ = (bitField0_ & ~0x00001000);
+        ip_ = getDefaultInstance().getIp();
+        onChanged();
+        return this;
+      }
+      /**
+       * <code>optional string ip = 13;</code>
+       */
+      public Builder setIpBytes(
+          com.google.protobuf.ByteString value) {
+        if (value == null) {
+    throw new NullPointerException();
+  }
+  bitField0_ |= 0x00001000;
+        ip_ = value;
+        onChanged();
+        return this;
       }
       public final Builder setUnknownFields(
           final com.google.protobuf.UnknownFieldSet unknownFields) {
@@ -10036,27 +10288,27 @@ public final class WolverineProto {
       "TaskMsg\022\016\n\006taskId\030\001 \002(\t\022\r\n\005jobId\030\002 \002(\t\022\033" +
       "\n\010taskType\030\003 \002(\0162\t.TaskType\022!\n\013commandTy" +
       "pe\030\004 \002(\0162\014.CommandType\022\033\n\010dataType\030\005 \002(\016" +
-      "2\t.DataType\022\014\n\004data\030\006 \001(\014\"\326\001\n\rCreateTask" +
+      "2\t.DataType\022\014\n\004data\030\006 \001(\014\"\360\001\n\rCreateTask" +
       "Msg\022\016\n\006taskId\030\001 \002(\t\022\r\n\005jobId\030\002 \001(\t\022\023\n\013im" +
       "ageAndTag\030\003 \001(\t\022\020\n\010imageUri\030\004 \001(\t\022\017\n\007com" +
       "mand\030\005 \001(\t\022\022\n\nentryPoint\030\006 \001(\t\022\013\n\003mem\030\007 " +
       "\001(\005\022\r\n\005cores\030\010 \001(\005\022\014\n\004disk\030\t \001(\005\022\030\n\007opti" +
       "ons\030\n \003(\0132\007.Option\022\026\n\006limits\030\013 \003(\0132\006.Lim" +
-      "it\"R\n\tTaskIdMsg\022\016\n\006taskId\030\001 \002(\t\022\r\n\005jobId" +
-      "\030\002 \002(\t\022\023\n\013containerId\030\003 \001(\t\022\021\n\tprocessId" +
-      "\030\004 \001(\t\"-\n\014HeartBeatMsg\022\016\n\006taskId\030\001 \002(\t\022\r" +
-      "\n\005jobId\030\002 \001(\t\"=\n\014PauseTaskMsg\022\016\n\006taskId\030" +
-      "\001 \002(\t\022\r\n\005jobId\030\002 \001(\t\022\016\n\006reason\030\003 \001(\t\"?\n\016" +
-      "RestartTaskMsg\022\016\n\006taskId\030\001 \002(\t\022\r\n\005jobId\030" +
-      "\002 \001(\t\022\016\n\006reason\030\003 \001(\t\"<\n\013KillTaskMsg\022\016\n\006" +
-      "taskId\030\001 \002(\t\022\r\n\005jobId\030\002 \001(\t\022\016\n\006reason\030\003 " +
-      "\001(\t*\"\n\010DataType\022\010\n\004JSON\020\000\022\014\n\010PROTOBUF\020\001*" +
-      "_\n\013CommandType\022\017\n\013CREATE_TASK\020\000\022\016\n\nHEART" +
-      "_BEAT\020\001\022\016\n\nPAUSE_TASK\020\002\022\020\n\014RESTART_TASK\020" +
-      "\003\022\r\n\tKILL_TASK\020\004*C\n\010TaskType\022\017\n\013DYNAMIC_" +
-      "LIB\020\000\022\017\n\013SPRING_BOOT\020\001\022\t\n\005SHELL\020\002\022\n\n\006DOC" +
-      "KER\020\003B$\n\022io.wolverine.protoB\016WolverinePr" +
-      "oto"
+      "it\022\014\n\004port\030\014 \001(\005\022\n\n\002ip\030\r \001(\t\"R\n\tTaskIdMs" +
+      "g\022\016\n\006taskId\030\001 \002(\t\022\r\n\005jobId\030\002 \002(\t\022\023\n\013cont" +
+      "ainerId\030\003 \001(\t\022\021\n\tprocessId\030\004 \001(\t\"-\n\014Hear" +
+      "tBeatMsg\022\016\n\006taskId\030\001 \002(\t\022\r\n\005jobId\030\002 \001(\t\"" +
+      "=\n\014PauseTaskMsg\022\016\n\006taskId\030\001 \002(\t\022\r\n\005jobId" +
+      "\030\002 \001(\t\022\016\n\006reason\030\003 \001(\t\"?\n\016RestartTaskMsg" +
+      "\022\016\n\006taskId\030\001 \002(\t\022\r\n\005jobId\030\002 \001(\t\022\016\n\006reaso" +
+      "n\030\003 \001(\t\"<\n\013KillTaskMsg\022\016\n\006taskId\030\001 \002(\t\022\r" +
+      "\n\005jobId\030\002 \001(\t\022\016\n\006reason\030\003 \001(\t*\"\n\010DataTyp" +
+      "e\022\010\n\004JSON\020\000\022\014\n\010PROTOBUF\020\001*_\n\013CommandType" +
+      "\022\017\n\013CREATE_TASK\020\000\022\016\n\nHEART_BEAT\020\001\022\016\n\nPAU" +
+      "SE_TASK\020\002\022\020\n\014RESTART_TASK\020\003\022\r\n\tKILL_TASK" +
+      "\020\004*C\n\010TaskType\022\017\n\013DYNAMIC_LIB\020\000\022\017\n\013SPRIN" +
+      "G_BOOT\020\001\022\t\n\005SHELL\020\002\022\n\n\006DOCKER\020\003B$\n\022io.wo" +
+      "lverine.protoB\016WolverineProto"
     };
     com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner assigner =
         new com.google.protobuf.Descriptors.FileDescriptor.    InternalDescriptorAssigner() {
@@ -10093,7 +10345,7 @@ public final class WolverineProto {
     internal_static_CreateTaskMsg_fieldAccessorTable = new
       com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
         internal_static_CreateTaskMsg_descriptor,
-        new java.lang.String[] { "TaskId", "JobId", "ImageAndTag", "ImageUri", "Command", "EntryPoint", "Mem", "Cores", "Disk", "Options", "Limits", });
+        new java.lang.String[] { "TaskId", "JobId", "ImageAndTag", "ImageUri", "Command", "EntryPoint", "Mem", "Cores", "Disk", "Options", "Limits", "Port", "Ip", });
     internal_static_TaskIdMsg_descriptor =
       getDescriptor().getMessageTypes().get(4);
     internal_static_TaskIdMsg_fieldAccessorTable = new
